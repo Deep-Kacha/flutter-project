c04cb0ceea78d640b777c911e023f242e433b67c
Deep-Kacha <deepnkacha2256@gmail.com>
2025-07-22 09:03:23 +0530

    -

---
+++ Flutter_NV/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_nv/calculator.dart';
+// import 'package:flutter_nv/containerExperiment.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: CalculatorPart());
+  }
+}
