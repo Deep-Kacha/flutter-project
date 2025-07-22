16e519bf0d87287834e278bf45747d5cfe450a86
Deep-Kacha <deepnkacha2256@gmail.com>
2025-07-22 09:42:33 +0530

    -

--- Flutter_NV/lib/main.dart
+++ Flutter_NV/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_nv/calculator.dart';
+// import 'package:flutter_nv/calculator.dart';
 // import 'package:flutter_nv/containerExperiment.dart';
+import 'package:flutter_nv/stopwatch.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,6 +12,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: CalculatorPart());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StopwatchExperiemnt(),
+    );
   }
 }
