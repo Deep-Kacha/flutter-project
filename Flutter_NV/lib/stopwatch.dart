36b9db325818c812b2e40299e82dac18c7332da9
Deep-Kacha <deepnkacha2256@gmail.com>
2025-07-29 08:35:27 +0530

    stop watch

--- Flutter_NV/lib/stopwatch.dart
+++ Flutter_NV/lib/stopwatch.dart
@@ -11,6 +11,7 @@ class StopwatchExperiemnt extends StatefulWidget {
 class _StopwatchExperiemntState extends State<StopwatchExperiemnt> {
   int seconds = 0;
   late Timer timer;
+  bool isTicking = false;
   String _secondtoText() => seconds <= 1 ? 'Second' : 'Seconds';
   @override
   Widget build(BuildContext context) {
@@ -27,26 +28,26 @@ class _StopwatchExperiemntState extends State<StopwatchExperiemnt> {
             ),
           ),
           const SizedBox(height: 10),
-          const Row(
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               ElevatedButton(
-                onPressed: null,
-                style: ButtonStyle(
+                onPressed: isTicking ? null : _starttimer,
+                style: const ButtonStyle(
                   backgroundColor: MaterialStatePropertyAll(Colors.green),
                   foregroundColor: MaterialStatePropertyAll(Colors.white),
                 ),
-                child: Text("Start"),
+                child: const Text("Start"),
               ),
-              SizedBox(width: 10),
+              const SizedBox(width: 10),
               ElevatedButton(
-                onPressed: null,
-                style: ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 255, 17, 0)),
+                onPressed: isTicking ? _stoptimer : null,
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.red),
                   foregroundColor: MaterialStatePropertyAll(Colors.white),
                 ),
-                child: Text("Stop"),
+                child: const Text("Stop"),
               ),
             ],
           ),
@@ -58,7 +59,21 @@ class _StopwatchExperiemntState extends State<StopwatchExperiemnt> {
   @override
   void initState() {
     super.initState();
+  }
+
+  void _starttimer() {
     timer = Timer.periodic(const Duration(seconds: 1), _onTick);
+    setState(() {
+      isTicking = true;
+      seconds = 0;
+    });
+  }
+
+  void _stoptimer() {
+    timer.cancel();
+    setState(() {
+      isTicking = false;
+    });
   }
 
   void _onTick(Timer timer) {
